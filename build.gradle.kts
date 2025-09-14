388a264d7151ccc5e22d5029ce527569316bf2a3
Karim Attia <mail+contact+github@karimattia.ch>
2025-09-14 18:25:05 +0200

    Update Gradle and wrapper to version 8.12.2 and 8.13 respectively, enhancing build tools and performance.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
 
-        classpath("com.android.tools.build:gradle:8.10.0")
+        classpath("com.android.tools.build:gradle:8.12.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.4")
 
